efe6ce32651269a309c4c590c40798844c2eda97
1 <ypopov2005@gmail.com>
2025-03-14 12:31:59 +0300

    update

--- lab_01/src/request.cpp
+++ lab_01/src/request.cpp
@@ -11,26 +11,26 @@ err_t request_handler(const request_t &request)
 
     switch (request.task)
     {
-        case REQ_LOAD:
-            rc = model_load(model, request.filename);
-            break;
-        case REQ_SHIFT:
-            rc = model_shift(model, request.shift);
-            break;
-        case REQ_TURN:
-            rc = model_turn(model, request.turn);
-            break;
-        case REQ_SCALE:
-            rc = model_scale(model, request.scale);
-            break;
-        case REQ_RENDER:
-            rc = model_render(request.render, model);
-            break;
-        case REQ_QUIT:
-            model_free(model);
-            break;
-        default:
-            rc = ERR_UNKNOWN_COMMAND;
+    case REQ_LOAD:
+        rc = model_load(model, request.filename);
+        break;
+    case REQ_SHIFT:
+        rc = model_shift(model, request.shift);
+        break;
+    case REQ_TURN:
+        rc = model_turn(model, request.turn);
+        break;
+    case REQ_SCALE:
+        rc = model_scale(model, request.scale);
+        break;
+    case REQ_RENDER:
+        rc = model_render(request.render, model);
+        break;
+    case REQ_QUIT:
+        model_free(model);
+        break;
+    default:
+        rc = ERR_UNKNOWN_COMMAND;
     }
 
     return rc;
